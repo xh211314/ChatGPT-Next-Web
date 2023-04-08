dcbdad0bda16b37caf57d2911325e6490c6df806
xh211314 <168377292@qq.com>
2023-04-08 21:02:01 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -158,7 +158,8 @@ function _Home() {
             </div>
             <div className={styles["sidebar-action"]}>
               <a href={REPO_URL} target="_blank">
-                <IconButton icon={<GithubIcon />} shadow />
+<!--                 <IconButton icon={<GithubIcon />} shadow /> -->
+                购买链接
               </a>
             </div>
           </div>
