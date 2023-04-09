f9a4ad542aef4ef1ea2c0e4f31902e0a25a4ee8f
xh211314 <168377292@qq.com>
2023-04-09 11:39:45 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/app";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请在设置页输入访问密码。",
+    Unauthorized: "账户未授权，请点击左下方开通账户获取授权码。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
