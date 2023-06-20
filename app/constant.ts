281daef85227c1d6a5374a4ec8d59dc5a51d1864
xh211314 <168377292@qq.com>
2023-06-20 15:10:47 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,6 @@
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
-export const BUY_URL = `https://blog.zhuliansoft.com/archives/832`;
+export const BUY_URL = `/buy.html`;
 export const HELP_URL = BUY_URL;
 export const REPO_URL = BUY_URL;
 export const ISSUE_URL = BUY_URL;
